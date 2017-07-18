01f4780130ee2adfe6ff0a8cd66350e71d1fcc4e
kbridson <kbridson@memphis.edu>
2017-07-18 18:07:43 -0500

    Step 3.7
    Save a New User Object

--- BucketListApp/app.py
+++ BucketListApp/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, json
-from models import db
+from models import *
 
 app = Flask(__name__)
 
@@ -23,7 +23,17 @@ def signUp():
  
     # validate the received values
     if _name and _email and _password:
-        return json.dumps({'html':'<span>All fields entered</span>'})
+        user = User.query.filter_by(user_email = _email.lower()).first()
+        if not user:
+            db.session.add(User(_name,_email,_password))
+            try:
+                db.session.commit()
+                return json.dumps({'html':'<span>User added</span>'})
+            except Exception as e:
+                db.session.rollback()
+                db.session.flush()
+                return json.dumps({'html':'<span>' + e + '</span>'})
+        return json.dumps({'html':'<span>Email not unique</span>'})
     else:
         return json.dumps({'html':'<span>Missing fields</span>'})
 
